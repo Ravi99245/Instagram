b30bf5318f328bbadc23cd7e6945df9b6db50bb6
Ravi99245 <vasamsettiravi5@gmail.com>
2024-06-06 07:17:33 +0000

    Login Design Mades

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,5 +1,15 @@
 import {Component} from 'react'
 
+import {
+  LoginImage,
+  InstaHeading,
+  LoginFormContainer,
+  FromElement,
+  InputElements,
+  InputElement,
+  LabelElement,
+} from './styledComponent'
+
 class LoginForm extends Component {
   state = {
     username: '',
@@ -9,24 +19,74 @@ class LoginForm extends Component {
     errorMsg: '',
   }
 
-  submitfrom = async event => {
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  submitForm = async event => {
     event.preventDefault()
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+
+    return (
+      <>
+        <LabelElement className="input-label" htmlFor="password">
+          PASSWORD
+        </LabelElement>
+        <InputElement
+          type="password"
+          id="password"
+          className="password-input-field"
+          value={password}
+          onChange={this.onChangePassword}
+          placeholder="Password"
+        />
+      </>
+    )
+  }
+
+  renderInputField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <LabelElement htmlFor="username">USERNAME</LabelElement>
+        <InputElement
+          type="text"
+          id="username"
+          value={username}
+          onChange={this.onChangeUsername}
+          placeholder="Username"
+        />
+      </>
+    )
   }
 
   render() {
     return (
-      <div>
+      <LoginFormContainer>
         <div>
-          <img
+          <LoginImage
             src="https://res.cloudinary.com/da0gwokvk/image/upload/v1717048613/pgperlq6gunnksf44qay.png"
             alt="input-logo"
           />
         </div>
-        <form onSubmit={this.submitfrom}>
-          <div>{this.renderInputField()}</div>
-          <div>{this.renderPasswordField()}</div>
-        </form>
-      </div>
+        <FromElement onSubmit={this.submitForm}>
+          <img
+            src="https://res.cloudinary.com/dxollddbj/image/upload/v1717655150/Standard_Collection_8_fskqkr.png"
+            alt="instaLogo"
+          />
+          <InstaHeading>Insta Share</InstaHeading>
+
+          <InputElements>{this.renderInputField()}</InputElements>
+          <InputElements>{this.renderPasswordField()}</InputElements>
+        </FromElement>
+      </LoginFormContainer>
     )
   }
 }
---
+++ src/components/LoginForm/styledComponent.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const LoginFormContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Roboto';
+`
+
+export const LoginImage = styled.img`
+  width: 582px;
+  height: 373px;
+  top: 326px;
+  left: 160px;
+  gap: 0px;
+  opacity: 0px;
+`
+
+export const InstaHeading = styled.h1`
+  font-size: 25px;
+`
+export const FromElement = styled.form`
+  width: 456px;
+  height: 458px;
+  gap: 0px;
+  border-radius: 8px 0px 0px 0px;
+  opacity: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const InstaLogo = styled.img`
+  width: 82px;
+  height: 42px;
+`
+export const InputElements = styled.div`
+  width: 360px;
+  height: 64px;
+  display: flex;
+  flex-direction: column;
+`
+export const InputElement = styled.input`
+  border: none;
+  margin-top: 6px;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: 0.01em;
+  text-align: left;
+  background-color: '#C8C8CF';
+`
+export const LabelElement = styled.label`
+  font-size: 16px;
+`
